2346cc8adef3d023c7485a2318bd35cb9f895a40
Yitian Li <98676784+macoyo2@users.noreply.github.com>
2022-02-28 03:43:48 -0500

    Update 00_data_preprocessing.R

--- scripts/00_data_preprocessing.R
+++ scripts/00_data_preprocessing.R
@@ -1,1 +1,35 @@
+### Preamble ###
+# Purpose: Clean the Daily Shelter occupancy data downloaded from Toronto Open Data
+# Author: 
+  - Yitian Li 
+  - Wei Zhang
+  - Nan An
+# Date: February 28 2021
+# Pre-req: None
 
+### Workspace Set-Up ###
+# install.packages("readxl")
+# install.packages("tidyverse")
+# install.packages("dplyr")
+# install.packages("ggpubr")
+# install.packages("ggplot2")
+# install.packages("astsa")
+# install.packages("lubridate")
+library(readxl)
+library(tidyverse)
+library(dplyr)
+library(ggpubr)
+library(ggplot2)
+library(astsa)
+library(lubridate)
+
+
+### Import the dataset ###
+raw_data<- read_excel("data.xlsx")
+
+### Clean data ###
+raw_data <- raw_data %>% filter(Year < 2017)
+names(raw_data)[2] = "Urban income"
+names(raw_data)[3] = "Rural income"                      
+names(raw_data)[5] = "Gini"
+attach(raw_data)
